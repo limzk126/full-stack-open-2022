c6aca6395f59de48083742b609a72d7dad6169ec
puzzlehead <dylanlim12698@gmail.com>
2022-07-30 23:27:52 +0800

    Upload solution for the phonebook 2.7

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -11,6 +11,11 @@ const App = () => {
 
   const addNewPerson = (event) => {
     event.preventDefault();
+    if(persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook!`);
+      return;
+    }
+
     setPersons(persons.concat({ name: newName }));
     setNewName("");
   };
