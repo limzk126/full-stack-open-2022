5bc2b3c08d250d860ec82cd34189ff9c6c21ef00
puzzlehead <dylanlim12698@gmail.com>
2022-07-30 23:47:32 +0800

    Upload solutions for the phonebook 2.8

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -5,19 +5,26 @@ import { v4 as uuid } from "uuid";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addNewPerson = (event) => {
     event.preventDefault();
-    if(persons.find((person) => person.name === newName)) {
+    if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook!`);
       return;
     }
 
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName , number: newNumber}));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -28,13 +35,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <p key={uuid()}>{person.name}</p>
+          <p key={uuid()}>{person.name} {person.number}</p>
         ))}
       </div>
     </div>
