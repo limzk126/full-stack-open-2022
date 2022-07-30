8ba4b1516d4bdef9514517028ca54933892e5815
puzzlehead <dylanlim12698@gmail.com>
2022-07-30 23:19:43 +0800

    Upload solutions for the phonebook 2.6

---
+++ part2/the-phonebook/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={uuid()}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
